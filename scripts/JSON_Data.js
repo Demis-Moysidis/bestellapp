0cfde1a9426bc452d072c6c237a43ddce8088d8f
Demis Moysidis <demis.moysidis@outlook.de>
2025-06-20 22:57:34 +0200

    first commit

---
+++ scripts/JSON_Data.js
@@ -0,0 +1,83 @@
+let products = {
+  "main_courses": [
+    {
+      "name": "Pizza Margherita",
+      "price": 8.5,
+      "description": "mit Tomaten und Mozzarella"
+    },
+    {
+      "name": "Pizza Salami",
+      "price": 9.5,
+      "description": "mit Salami und Käse"
+    },
+    {
+      "name": "Pizza Funghi",
+      "price": 9.0,
+      "description": "mit frischen Champignons"
+    },
+    {
+      "name": "Pizza Hawaii",
+      "price": 10.0,
+      "description": "mit Ananas und Schinken"
+    },
+    {
+      "name": "Pizza Diavolo",
+      "price": 10.5,
+      "description": "mit scharfer Salami und Peperoni"
+    }
+  ],
+  "side_dishes": [
+    {
+      "name": "Pommes Frites",
+      "price": 3.5,
+      "description": "mit Ketchup oder Mayo"
+    },
+    {
+      "name": "Mozzarella Sticks",
+      "price": 4.5,
+      "description": "mit Käsefüllung und Knoblauchdip"
+    },
+    {
+      "name": "Bruschetta",
+      "price": 4.0,
+      "description": "mit Tomaten und Basilikum"
+    },
+    {
+      "name": "Salat klein",
+      "price": 3.0,
+      "description": "mit Essig-Öl-Dressing"
+    },
+    {
+      "name": "Knoblauchbrot",
+      "price": 2.5,
+      "description": "mit frischem Knoblauch"
+    }
+  ],
+  "drinks": [
+    {
+      "name": "Cola",
+      "price": 2.5,
+      "description": "mit Kohlensäure"
+    },
+    {
+      "name": "Wasser still",
+      "price": 2.0,
+      "description": "mit wenig Mineralien"
+    },
+    {
+      "name": "Apfelschorle",
+      "price": 2.5,
+      "description": "mit Apfelsaft und Sprudel"
+    },
+    {
+      "name": "Orangensaft",
+      "price": 2.8,
+      "description": "mit Fruchtfleisch"
+    },
+    {
+      "name": "Bier",
+      "price": 3.0,
+      "description": "mit Hopfen und Malz"
+    }
+  ]
+}
